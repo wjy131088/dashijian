5e619832d35a216e93f10f0138959402b6e71bc8
wangjianyun <wjy131088@gmail.com>
2022-08-04 11:19:47 +0800

    index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,5 +4,29 @@
 const baseURL = "http://www.liulongbin.top:3007";
 $.ajaxPrefilter((option) => {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  if (option.url.includes("/my")) {
+    option.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
   option.url = baseURL + option.url;
+
+  // 不论成功还是失败，最终都会调用 complete 回调函数
+  option.complete = (res) => {
+    console.log(res);
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      // console.log('没有token');
+      // 强制跳转到登录页面
+      location.href = "/login.html";
+      //  强制清空 token
+      localStorage.removeItem("token");
+   
+    }/* else{
+      console.log('有token');
+    } */
+  };
 });
---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+const getUserInfo = function () {
+  $.ajax({
+    type: "GET",
+    url: "/my/userinfo",
+    data: null,
+    success: function (res) {
+      //   console.log(res);
+      const { status, message } = res;
+      if (status !== 0) return layer.msg(message);
+      renderAvatar(res.data);
+    },
+  });
+};
+const renderAvatar = (data) => {
+  let name = data.nickname || data.username;
+  // console.log(name);
+  // 设置欢迎文本
+  $("#welcom").html("欢迎" + name);
+  // 按需渲染用户头像
+  if (data.user_pic !== null) {
+    // 渲染图片头像
+    $(".layui-nav-img").attr("src", data.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+    // 渲染文本头像
+    $(".layui-nav-img").hide();
+    let firstName = name[0].toUpperCase();
+    $(".text-avatar").html(firstName);
+  }
+};
+getUserInfo();
+$("#exitBtn").click(() => {
+  layer.confirm("确定退出?", { icon: 3, title: "提示" }, function (index) {
+    //do something
+    // 清空本地存储里面的 token
+    localStorage.removeItem("token");
+    // 重新跳转到登录页面
+    location.href = "/login.html";
+    layer.close(index);
+  });
+});
